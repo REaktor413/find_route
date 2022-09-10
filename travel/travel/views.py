3de85496942b465184ca8e09721bb75d90ef67bc
Khoroshilov <khoroshilov.k.n@gmail.com>
2022-09-10 08:41:38 +0300

    try extends template

--- travel/travel/views.py
+++ travel/travel/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 def home(request):
     name = "Bob"
 
-    return render(request, 'home.html', context={"name":name})
+    return render(request, 'home.html', context={"name": name})
+
+def about(request):
+    name = "About us"
+
+    return render(request, 'about.html', context={"name": name})
